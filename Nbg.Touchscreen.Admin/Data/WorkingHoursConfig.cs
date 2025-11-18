4777e3ef9c87b92ab84ac56e7775e2344e898437
Steviamp <amplianitistavr@outlook.com>
2025-11-18 13:42:27 +0200

    dates

---
+++ Nbg.Touchscreen.Admin/Data/WorkingHoursConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nbg.Touchscreen.Admin.Data;
+
+public class WorkingHoursConfig
+{
+    public List<DaySchedule> Days { get; set; } = new();
+    public List<DateOnly> BankHolidays { get; set; } = new();
+}
+
+public class DaySchedule
+{
+    public DayOfWeek Day { get; set; }
+    public bool Enabled { get; set; }
+    public TimeSpan? From { get; set; }
+    public TimeSpan? To { get; set; }
+}
