ba2969ce1d3fa6b85536beeff579a88b2ec9eb66
Steviamp <amplianitistavr@outlook.com>
2025-11-24 14:48:59 +0200

    remove hours from pharmacies

--- Nbg.Touchscreen.Admin/Data/Pharmacy.cs
+++ Nbg.Touchscreen.Admin/Data/Pharmacy.cs
@@ -18,34 +18,6 @@
         public string? PharmacyIP { get; set; }
         public int? Port { get; set; }
         public DateTime CreatedAtUtc { get; set; }
-        public bool MondayEnabled { get; set; }
-        public TimeSpan? MondayFrom { get; set; }
-        public TimeSpan? MondayTo { get; set; }
-
-        public bool TuesdayEnabled { get; set; }
-        public TimeSpan? TuesdayFrom { get; set; }
-        public TimeSpan? TuesdayTo { get; set; }
-
-        public bool WednesdayEnabled { get; set; }
-        public TimeSpan? WednesdayFrom { get; set; }
-        public TimeSpan? WednesdayTo { get; set; }
-
-        public bool ThursdayEnabled { get; set; }
-        public TimeSpan? ThursdayFrom { get; set; }
-        public TimeSpan? ThursdayTo { get; set; }
-
-        public bool FridayEnabled { get; set; }
-        public TimeSpan? FridayFrom { get; set; }
-        public TimeSpan? FridayTo { get; set; }
-
-        public bool SaturdayEnabled { get; set; }
-        public TimeSpan? SaturdayFrom { get; set; }
-        public TimeSpan? SaturdayTo { get; set; }
-
-        public bool SundayEnabled { get; set; }
-        public TimeSpan? SundayFrom { get; set; }
-        public TimeSpan? SundayTo { get; set; }
-        public string? BankHolidaysJson { get; set; }
         public int? PrefectureId { get; set; }
         public Prefecture? Prefecture { get; set; }
         public TicketingProvider TicketingProvider { get; set; } = TicketingProvider.None;
